809bb3871521ca7c627b4f6757c4e345403c66a2
Leah Lundqvist <leah@pigeon.sh>
2025-03-14 14:10:40 +0100

    add cost_per_gpu_hour

--- src/orm/crg.rs
+++ src/orm/crg.rs
@@ -4,6 +4,7 @@ impl_orm_object!(
     ComputeResourceGroup,
     "compute_resource_group",
     cost_per_cpu_hour: BigDecimal,
+    cost_per_gpu_hour: BigDecimal,
     cost_per_gb_hour: BigDecimal,
     cost_per_net_rx_gb: BigDecimal,
     cost_per_net_tx_gb: BigDecimal,
